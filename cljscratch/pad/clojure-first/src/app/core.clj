8e562f32e6bb92407a96e7fc06a4cf871ee919f1
Jake Bladt <jake@jakebladt.com>
2014-03-01 10:31:41 -0500

    fiddling with lein/clojure

---
+++ cljscratch/pad/clojure-first/src/app/core.clj
@@ -0,0 +1,7 @@
+(ns clojure-first.core
+  (:gen-class :name FirstClass :prefix FirstClass-))
+
+(defn FirstClass-main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "I'm a little teapot!"))
