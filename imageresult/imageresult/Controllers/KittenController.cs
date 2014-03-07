63202e4b7ee56ada80edd04c1c53b81e60d222d8
Jake Bladt <jake@jakebladt.com>
2014-03-06 21:49:59 -0500

    added 'image result' project

---
+++ imageresult/imageresult/Controllers/KittenController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace imageresult.Controllers
+{
+    public class KittenController : Controller
+    {
+        public ActionResult Image(string id)
+        {
+            var imgDir = Server.MapPath(@"/Content/Images");
+            var path = Path.Combine(imgDir, id + ".jpg");
+            return File(path, "image/jpeg"); 
+        }
+
+    }
+}
