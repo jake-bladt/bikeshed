46132092b5965408d0c27b802e5a62b9e3aa42af
Jake Bladt <jake@jakebladt.com>
2014-03-18 07:20:57 -0500

    create Scrubber

---
+++ Gallery/GalleryScrubber/Scrubber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ImageGallery;
+
+namespace GalleryScrubber
+{
+    public class Scrubber
+    {
+        protected ImageGallery.ImageGallery _gallery;
+        public Scrubber(ImageGallery.ImageGallery gallery) 
+        { 
+            _gallery = gallery;  
+        }
+    }
+}
