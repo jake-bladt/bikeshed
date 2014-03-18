840d73379c7bf375663c255ac6868e45481ff62c
Jake Bladt <jake@jakebladt.com>
2014-03-18 07:36:20 -0500

    create subject

---
+++ Gallery/ImageGallery/Subject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageGallery
+{
+    public class Subject
+    {
+        public string DirectoryPath { get; protected set; }
+        public string Name { get; protected set; }
+
+        public Subject(string path, string name)
+        {
+            DirectoryPath = path;
+            Name = name;
+        }
+    }
+}
