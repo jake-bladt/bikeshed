5235db225382c12848eff37933e47b993df0d235
Jake Bladt <jake@jakebladt.com>
2014-03-18 20:19:41 -0500

    invoke scrubber

--- Gallery/scrub/Program.cs
+++ Gallery/scrub/Program.cs
@@ -14,11 +14,28 @@ namespace scrub
     {
         static void Main(string[] args)
         {
-            var galleryPath = ConfigurationManager.AppSettings["GallerySource"];
-            var gallery = new ImageGallery.ImageGallery(galleryPath);
-            var scrubber = new Scrubber(gallery);
-            
+            try
+            {
+                var galleryPath = ConfigurationManager.AppSettings["GallerySource"];
+                var gallery = new ImageGallery.ImageGallery(galleryPath);
+                var scrubber = new Scrubber(gallery);
 
+                if (args.Length > 0)
+                {
+                    string target = args[0].ToLower();
+                    var ret = scrubber.ScrubSubject(target);
+                    Console.WriteLine(ret);
+                }
+                else
+                {
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
             Console.ReadLine();
         }
     }
