667fa31cee57bc303752cce44b82fed6142e02f4
Jake Bladt <jake@jakebladt.com>
2014-03-28 07:15:53 -0400

    subject date added

--- Gallery/SQLDatabase/dbo.subjects.sql
+++ Gallery/SQLDatabase/dbo.subjects.sql
@@ -1,7 +1,9 @@
-﻿CREATE TABLE [dbo].[Subjects]
-(
-	[Id] INT IDENTITY NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(50) NOT NULL, 
-    [DisplayName] VARCHAR(50) NOT NULL, 
-    [ImageCount] INT NOT NULL
-)
+﻿CREATE TABLE [dbo].[Subjects] (
+    [Id]          INT          IDENTITY (1, 1) NOT NULL,
+    [Name]        VARCHAR (50) NOT NULL,
+    [DisplayName] VARCHAR (50) NOT NULL,
+    [ImageCount]  INT          NOT NULL,
+    [DateAdded] DATETIME NOT NULL DEFAULT GetDate(), 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
