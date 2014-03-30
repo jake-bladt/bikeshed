0df4227507ed9aca9a9cc590680d13d99327f300
Jake Bladt <jake@jakebladt.com>
2014-03-29 23:26:53 -0400

    election store skel

---
+++ Gallery/Gallery.Entities/Elections/SqlBackedElectionSet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Gallery.Entities.Elections
+{
+    public class SqlBackedElectionSet
+    {
+        public string ConnectionString { get; protected set; }
+
+        public SqlBackedElectionSet(String cn)
+        {
+            ConnectionString = cn;
+        }
+
+        public bool Store(Election election)
+        {
+            var cn = new SqlConnection(ConnectionString);
+            cn.Open();
+
+            if (ElectionExists(election))
+            {
+
+            }
+            else
+            {
+                
+            }
+
+            election.Winners.ToList().ForEach(kvp =>
+            {
+
+            });
+
+            return true;
+        }
+
+        public bool ElectionExists(Election election)
+        {
+            var cn = new SqlConnection(ConnectionString);
+            cn.Open();
+            var cmd = new SqlCommand("getElection", cn) { CommandType = CommandType.StoredProcedure };
+            var rdr = cmd.ExecuteReader();
+            return rdr.Read();
+        }
+    }
+}
