1b03af0bf77273ca7fcb6bedfc41c0aa1bdded19
Jake Bladt <jake@jakebladt.com>
2014-04-01 21:24:47 -0400

    new app

---
+++ Gallery/rosubj/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace rosubj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dirPath = args[0];
+
+        }
+    }
+}
