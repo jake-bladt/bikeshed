783badd69ff59d7f3344e763b0744d927d6c38f6
Jake Bladt <jake@jakebladt.com>
2014-04-01 21:42:03 -0400

    subject randomization utility

--- Gallery/rosubj/Program.cs
+++ Gallery/rosubj/Program.cs
@@ -13,6 +13,24 @@ namespace rosubj
         static void Main(string[] args)
         {
             var dirPath = args[0];
+            var di = new DirectoryInfo(dirPath);
+            var fis = di.GetFiles("*.jpg");
+            string pattern = @"^x\d{6}-";
+            var random = new Random();
+
+            fis.ToList().ForEach(fi =>
+            {
+                if (!Regex.IsMatch(fi.Name, pattern))
+                {
+                    int prefixNum = random.Next(0, 50000) + 50000;
+                    var newFileName = String.Format("x{0}-{1}", prefixNum, fi.Name);
+                    var newFilePath = Path.Combine(di.FullName, newFileName);
+                    Console.WriteLine(String.Format("{0} -> {1}", fi.Name, newFileName));
+                    File.Move(fi.FullName, newFilePath);
+                }
+            });
+
+            Console.ReadLine();
 
         }
     }
