93007226d31614076d81c1c84f43963dc49ce837
Jake Bladt <jake@jakebladt.com>
2014-04-04 19:59:14 -0400

    simple spec

---
+++ playerGrid/playerGrid/Scripts/Specs/app_spec.js
@@ -0,0 +1,6 @@
+﻿describe("The application", function () {
+    it("calculates batting average", function () {
+        var ba = app.battingAverage(5.0, 10.0);
+        expect(ba).toEqual(0.5);
+    });
+});
