f071aa9f2fa8e748d39cec62aacc2fa984212c68
Jake Bladt <jake@jakebladt.com>
2014-04-09 21:08:44 -0400

    initialize sql setlist

--- Gallery/Gallery.Entities/SetLists/SqlBackedSetlist.cs
+++ Gallery/Gallery.Entities/SetLists/SqlBackedSetlist.cs
@@ -10,12 +10,28 @@ namespace Gallery.Entities.SetLists
 {
     public class SqlBackedSetlist : SetList
     {
-        public static SqlBackedSetlist CloneFrom(SetList source) 
+        public string ConnectionString { get; protected set; }
+
+        public static SqlBackedSetlist CloneFrom(SetList source, string cn = "") 
         {
-            var ret = new SqlBackedSetlist() { Name = source.Name };
+            var ret = new SqlBackedSetlist(cn, source.Name);
             source.ToList().ForEach(kvp => ret[kvp.Key] = kvp.Value);
             return ret;
         }
 
+        public SqlBackedSetlist(string cn, string name) : base(name)
+        {
+            ConnectionString = cn;
+        }
+
+        public bool Store()
+        {
+            return false;
+        }
+
+        public bool Fetch()
+        {
+            return false;
+        }
     }
 }
