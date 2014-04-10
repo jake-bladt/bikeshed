67d3238b182fb42ba8f69c35c0bbdb2cfbfcd853
Jake Bladt <jake@jakebladt.com>
2014-04-09 22:07:10 -0400

    setlist app

---
+++ Gallery/setlist/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Gallery.Entities.Elections;
+
+namespace setlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string listName = args[0];
+            var elections = new List<IElection>();
+
+            for (int i = 1; i < args.Length; i++)
+            {
+
+            }
+        }
+    }
+}
