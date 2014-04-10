8c99e4b61e8c096d3e950786f0c1d0da88d3d1ea
Jake Bladt <jake@jakebladt.com>
2014-04-09 20:48:54 -0400

    set list from elections

---
+++ Gallery/Gallery.Entities/SetLists/SetList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Gallery.Entities.Elections;
+using Gallery.Entities.Subjects;
+
+namespace Gallery.Entities.SetLists
+{
+    public class SetList : Dictionary<String, ISubject>
+    {
+        public static SetList FromElections(IEnumerable<Election> elections)
+        {
+            var ret = new SetList();
+            elections.ToList().ForEach(el =>
+            {
+                el.Winners.Values.ToList().ForEach(winner =>
+                {
+                    if (!ret.ContainsKey(winner.Name)) ret[winner.Name] = winner;
+                });
+            });
+            return ret;
+        }
+
+    }
+
+}
