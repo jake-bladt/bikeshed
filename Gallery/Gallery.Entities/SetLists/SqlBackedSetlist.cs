2684bbdcd892fda804dbeba1b1f26ffccd06dedd
Jake Bladt <jake@jakebladt.com>
2014-04-09 21:58:41 -0400

    sql for setlist

--- Gallery/Gallery.Entities/SetLists/SqlBackedSetlist.cs
+++ Gallery/Gallery.Entities/SetLists/SqlBackedSetlist.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 using Gallery.Entities.Subjects;
 
@@ -12,10 +13,24 @@ namespace Gallery.Entities.SetLists
     {
         public string ConnectionString { get; protected set; }
 
-        public static SqlBackedSetlist CloneFrom(SetList source, string cn = "") 
+        public static SqlBackedSetlist CloneFrom(SetList source, string cn) 
         {
             var ret = new SqlBackedSetlist(cn, source.Name);
             source.ToList().ForEach(kvp => ret[kvp.Key] = kvp.Value);
+            return ret;
+        }
+
+        public static SqlBackedSetlist ByName(string name, string cnStr)
+        {
+            var ret = new SqlBackedSetlist(cnStr, name);
+            ret.Fetch();
+            return ret;
+        }
+
+        public static SqlBackedSetlist ById(int Id, string cnStr)
+        {
+            var ret = new SqlBackedSetlist(cnStr, String.Empty);
+            ret.Fetch(Id);
             return ret;
         }
 
@@ -24,14 +39,67 @@ namespace Gallery.Entities.SetLists
             ConnectionString = cn;
         }
 
-        public bool Store()
+        public bool Fetch(int? id = null)
         {
-            return false;
+            var cn = new SqlConnection(ConnectionString);
+            cn.Open();
+            var cmd = new SqlCommand("getSubjectSetMembers", cn) { CommandType = CommandType.StoredProcedure };
+            if (null == id)
+            {
+                cmd.Parameters.Add(new SqlParameter("setName", Name));
+            }
+            else
+            {
+                cmd.Parameters.Add(new SqlParameter("setId", id));
+            }
+            var rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                var subject = new SqlTrackedSubject
+                {
+                    ID = (int)rdr["Id"],
+                    Name = (string)rdr["Name"],
+                    DisplayName = (string)rdr["DisplayName"],
+                    ImageCount = (int)rdr["IamgeCount"]
+                };
+                this[subject.Name] = subject;
+            }
+            return true;
         }
 
-        public bool Fetch()
+        public bool Store()
         {
-            return false;
+            return StoreSet() && StoreMembers();
         }
+
+        protected bool StoreSet()
+        {
+            var cn = new SqlConnection(ConnectionString);
+            cn.Open();
+            var cmd = new SqlCommand("addSubjectSet", cn) { CommandType = CommandType.StoredProcedure };
+            cmd.Parameters.Add(new SqlParameter("name", Name));
+            var ret = cmd.ExecuteScalar();
+            Id = Convert.ToInt32(ret);
+            return true;
+        }
+
+        protected bool StoreMembers()
+        {
+            var ret = true;
+            Values.ToList().ForEach(subj => ret &= StoreMember(subj));
+            return ret;
+        }
+
+        protected bool StoreMember(ISubject subject)
+        {
+            var cn = new SqlConnection(ConnectionString);
+            cn.Open();
+            var cmd = new SqlCommand("addSubjectSetMember", cn) { CommandType = CommandType.StoredProcedure };
+            cmd.Parameters.Add(new SqlParameter("setId", Id));
+            cmd.Parameters.Add(new SqlParameter("subjectId", subject.ID));
+            cmd.ExecuteNonQuery();
+            return true;
+        }
+    
     }
 }
