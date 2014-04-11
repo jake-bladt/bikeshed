eed705a74a190e873a34065e3122c41d080e3673
Jake Bladt <jake@jakebladt.com>
2014-04-11 12:14:26 -0400

    subject writer

---
+++ Gallery/Gallery.Entities/Subjects/SqlSubjectWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Gallery.Entities.Subjects
+{
+    public class SqlSubjectWriter : ISubjectWriter
+    {
+        public string ConnectionString { get; protected set; }
+
+        public SqlSubjectWriter(string cnStr)
+        {
+            ConnectionString = cnStr;
+        }
+
+        public bool UpdateImageCount(string name, int imageCount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
