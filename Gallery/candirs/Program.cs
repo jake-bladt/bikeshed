0f5292013d43873bd94447cee5d125238afc9be0
Jake Bladt <jake@jakebladt.com>
2014-04-22 16:35:34 -0400

    interim commit

--- Gallery/candirs/Program.cs
+++ Gallery/candirs/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.IO;
@@ -14,8 +15,9 @@ namespace candirs
         static void Main(string[] args)
         {
             string rootPath = args[0];
-            
-
+            string cn = ConfigurationManager.ConnectionStrings["galleryDb"].ConnectionString;
+            var gallery = new SqlTrackedImageGallery(cn);
+            var pool = SubjectPool.FromGallery(gallery);
         }
     }
 }
