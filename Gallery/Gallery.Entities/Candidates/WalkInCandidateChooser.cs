d561399d0553160ff92514bed1bca7bc9a8ede93
Jake Bladt <jake@jakebladt.com>
2014-11-29 19:26:01 -0500

    random chooser

---
+++ Gallery/Gallery.Entities/Candidates/WalkInCandidateChooser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Gallery.Entities.Subjects;
+
+namespace Gallery.Entities.Candidates
+{
+    public class WalkInCandidateChooser : ICandidateChooser 
+    {
+        protected ICandidatePool _pool;
+        protected double _targetCount;
+
+        public WalkInCandidateChooser(ICandidatePool pool, double targetCount)
+        {
+            _pool = pool;
+            _targetCount = targetCount;
+        }
+
+        public List<ISubject> GetCandidates()
+        {
+            var ret = new List<ISubject>();
+            var chance = _targetCount / _pool.Candidates.Count();
+            var rng = new Random();
+            _pool.Candidates.ForEach(c =>
+            {
+                if (chance < rng.NextDouble()) ret.Add(c);
+            });
+            return ret;
+        }
+
+        public string Name { get; protected set; }
+    }
+}
