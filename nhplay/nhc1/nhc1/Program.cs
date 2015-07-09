123e163adb862326ee139ecf938e56046a46dd99
Jake <jake@jakebladt.com>
2015-07-09 17:37:09 -0400

    create user

--- nhplay/nhc1/nhc1/Program.cs
+++ nhplay/nhc1/nhc1/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 
 using FluentNHibernate.Cfg;
 using FluentNHibernate.Cfg.Db;
 using NHibernate;
 using NHibernate.Cfg;
+using NHibernate.Linq;
 using NHibernate.Tool.hbm2ddl;
 
 namespace nhc1
@@ -16,30 +18,44 @@ namespace nhc1
         {
             // Create database if it doesn't exist
             CreateDB();
+            var user = CreateUser();
+
 
             Console.ReadLine();
         }
 
+        public static User CreateUser()
+        {
+            var factory = CreateSessionFactory();
+            using (var session = factory.OpenSession())
+            {
+                var ret = session.Get<User>(1);
+                if (null == ret)
+                {
+                    ret = new User { DisplayName = "jake" };
+                    session.Save(ret);
+                }
+                return ret;
+            }
+        }
+
         public static void CreateDB()
         {
-            var cnStr = 
-            Fluently.Configure()
-                .Database(MsSqlConfiguration
-                  .MsSql2012
-                  .ConnectionString(ConnectionString))
-                  .Mappings(m => m.FluentMappings.AddFromAssemblyOf<UserMap>())
-                  .ExposeConfiguration(c => new SchemaUpdate(c).Execute(true, true))
-                  .BuildSessionFactory();
+            GetDBConfig().ExposeConfiguration(c => new SchemaUpdate(c).Execute(true, true)).BuildSessionFactory();
         }
 
         public static ISessionFactory CreateSessionFactory()
+        {
+            return GetDBConfig().BuildSessionFactory();
+        }
+
+        public static FluentConfiguration GetDBConfig()
         {
             return Fluently.Configure()
                 .Database(MsSqlConfiguration
                   .MsSql2012
                   .ConnectionString(ConnectionString))
-                  .Mappings(m => m.FluentMappings.AddFromAssemblyOf<UserMap>())
-                  .BuildSessionFactory();
+                  .Mappings(m => m.FluentMappings.AddFromAssemblyOf<UserMap>());
         }
     }
 }
