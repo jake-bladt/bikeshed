54497b1c89b24c653f766d0e748a9dbfe0765b3b
Jake <jake@jakebladt.com>
2016-01-06 22:23:43 -0500

    +runoffs

--- Gallery/runoffs/Program.cs
+++ Gallery/runoffs/Program.cs
@@ -20,7 +20,27 @@ namespace runoffs
             
             string poolRoot = ConfigurationManager.AppSettings["yearbookLocation"];
             var poolInfo = new DirectoryInfo(poolRoot);
+            var pool = CandidatePool.FromFileSystemDirectory(poolInfo);
+            var chooser = new WalkInCandidateChooser(pool, targetCount * contestCount);
 
+            var candidates = chooser.GetCandidates();
+            var candidatesArray = candidates.ToArray();
+            for (int i = 1; i <= contestCount; ++i)
+            {
+                var runoffPath = Path.Combine(runoffRoot, i.ToString("000"));
+                for (int j = 1; j <= targetCount; ++j)
+                {
+                    var targetCandidateSlot = (i - 1) * targetCount + j - 1;
+                    if (candidatesArray.Length > targetCandidateSlot)
+                    {
+                        var candy = candidatesArray[targetCandidateSlot];
+                        var targetLocation = Path.Combine(runoffPath, candy.DisplayName);
+                        Console.WriteLine(String.Format("Copying {0} to {1}", candy.Name, targetLocation));
+                    }
+                }
+            }
+
+            Console.ReadLine();
 
         }
     }
