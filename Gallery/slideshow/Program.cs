8ddbaab7d6a471b0058adddb50cc1f177bbc5c1c
Jake Bladt <jake@jakebladt.com>
2016-11-02 03:36:58 -0400

    constrain to 32-bit

--- Gallery/slideshow/Program.cs
+++ Gallery/slideshow/Program.cs
@@ -15,7 +15,7 @@ namespace slideshow
         {
             string setName = args[0];
             string showName = args[1];
-            int imageCount = Int32.Parse(args[2]);
+            Int32 imageCount = Int32.Parse(args[2]);
             string cnStr = ConfigurationManager.ConnectionStrings["galleryDb"].ConnectionString;
             string subjectRoot = ConfigurationManager.AppSettings["subjectLocation"];
             string slideshowRoot = ConfigurationManager.AppSettings["slideshowLocation"];
