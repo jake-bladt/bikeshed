3e5f3e047217ca83cb226df9095827c2ec7057cb
Jake Bladt <jake@jakebladt.com>
2016-11-12 01:36:33 -0500

    +star chooser

---
+++ Gallery/Gallery.Entities/Candidates/StarChooser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+using Gallery.Entities.Subjects;
+
+namespace Gallery.Entities.Candidates
+{
+    public class StarChooser : ICandidateChooser
+    {
+        protected string ConnectionString { get; set; }
+
+        public StarChooser(string cn)
+        {
+            ConnectionString = cn;
+        }
+
+        public List<ISubject> GetCandidates()
+        {
+            var ret = new List<ISubject>();
+
+            var cn = new SqlConnection(ConnectionString);
+            cn.Open();
+
+            var cmd = new SqlCommand("getStars", cn) { CommandType = CommandType.StoredProcedure };
+            var rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                var subject = new SqlBackedSubject
+                {
+                    ID = Convert.ToInt32(rdr["Id"]),
+                    Name = rdr["Name"].ToString(),
+                    DisplayName = rdr["DisplayName"].ToString(),
+                    ImageCount = Convert.ToInt32(rdr["ImageCount"])
+                };
+                ret.Add(subject);
+            }
+
+            return ret;
+        }
+
+        public string Name { get; set; }
+    }
+}
