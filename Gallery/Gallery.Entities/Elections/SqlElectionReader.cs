c295dae4a867afbf969307b7c280cb1f7aee62d6
Jake Bladt <jake@jakebladt.com>
2016-12-04 14:47:42 -0500

    +impl get listing

--- Gallery/Gallery.Entities/Elections/SqlElectionReader.cs
+++ Gallery/Gallery.Entities/Elections/SqlElectionReader.cs
@@ -34,7 +34,16 @@ namespace Gallery.Entities.Elections
         public List<IElectionListing> GetElectionListing(int count)
         {
             var ret = new List<IElectionListing>();
-
+            var cn = new SqlConnection(_ConnectionString);
+            cn.Open();
+            var cmd = new SqlCommand("listElections", cn) { CommandType = CommandType.StoredProcedure };
+            cmd.Parameters.Add(new SqlParameter("count", count));
+            var rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                var listing = new ElectionListing { Name = (String)rdr["Name"], Id = (int)rdr["Id"] }; 
+                ret.Add(listing);
+            }
             return ret;
         }
 
