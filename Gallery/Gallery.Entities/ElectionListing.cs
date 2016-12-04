5b47a72b32831b762fbafc5736a295cb8ca4ada1
Jake Bladt <jake@jakebladt.com>
2016-12-04 14:39:01 -0500

    -unused code

---
+++ Gallery/Gallery.Entities/ElectionListing.cs
@@ -0,0 +1,8 @@
+﻿namespace Gallery.Entities.Elections
+{
+    public class ElectionListing: IElectionListing
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
