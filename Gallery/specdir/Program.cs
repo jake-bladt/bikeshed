f342a254551aeb67bf3ec4b260d8481b579ec271
Jake Bladt <jake@jakebladt.com>
2016-12-04 22:10:21 -0500

    generate special election

---
+++ Gallery/specdir/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+
+using Gallery.Entities.Candidates;
+using Gallery.Entities.ImageGallery;
+
+namespace specdir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contestPath = args[0];
+            string setName = args[1];
+
+            string cn = ConfigurationManager.ConnectionStrings["galleryDb"].ConnectionString;
+            string poolRoot = ConfigurationManager.AppSettings["yearbookLocation"];
+
+            var gallery = new SqlTrackedImageGallery(cn);
+            var pool = CandidatePool.FromGallery(gallery);
+
+            var chooser = new SetCandidateChooser(args[1], cn) { Name = setName };
+            var choosers = new ICandidateChooser[] { chooser };
+            var registrar = new ContestCandidateRegistrar(pool, choosers);
+            var candidateSet = registrar.GetContestCandidates();
+            var writer = new FileSystemContestWriter(contestPath, poolRoot);
+            var msg = writer.WriteContests(candidateSet) ? "Write complete." : "Write failed.";
+            Console.WriteLine(msg);
+            Console.ReadLine();
+        }
+    }
+}
