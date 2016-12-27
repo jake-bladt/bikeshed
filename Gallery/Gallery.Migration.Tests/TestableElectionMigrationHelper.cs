10034ac1749aa32bb2c476a4229f7ee7f890f12e
Jake Bladt <jake@jakebladt.com>
2016-12-27 06:53:55 -0500

    inherit from helper

--- Gallery/Gallery.Migration.Tests/TestableElectionMigrationHelper.cs
+++ Gallery/Gallery.Migration.Tests/TestableElectionMigrationHelper.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Linq;
+using Shouldly;
 using Xunit;
+
+using Gallery.Migration;
 
 namespace Gallery.Migration.Tests
 {
-    public class TestableElectionMigrationHelper
+    public class TestableElectionMigrationHelper : ElectionMigrationHelper
     {
+        
+
         [Fact]
-        public void TestMethod1()
+        public void TestSpecialElectionTitleDoesntTruncate()
         {
+           
         }
     }
 }
