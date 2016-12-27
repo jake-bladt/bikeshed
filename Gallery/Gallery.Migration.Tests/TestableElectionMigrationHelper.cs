55ffa0975f1eab062558f9fc1a3a542b0c727cba
Jake Bladt <jake@jakebladt.com>
2016-12-27 06:47:37 -0500

    +gallery tests

---
+++ Gallery/Gallery.Migration.Tests/TestableElectionMigrationHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Gallery.Migration.Tests
+{
+    public class TestableElectionMigrationHelper
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
