70093963ca3c4a2176cdd683ec7815ee096c53e0
Jake Bladt <jake@jakebladt.com>
2017-11-14 20:54:01 -0500

    +test code

--- Gallery/annual/Program.cs
+++ Gallery/annual/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace annual
 {
@@ -12,7 +12,13 @@ namespace annual
         {
             if(args.Length == 2)
             {
+                var year = args[0];
+                var path = args[1];
+                var subjects = GetYearDisplayNames(year, 
+                    ConfigurationManager.ConnectionStrings["galleryDb"].ConnectionString);
+                subjects.ForEach(s => Console.WriteLine(s));
 
+                Console.ReadLine();
             }
             else
             {
@@ -23,6 +29,23 @@ namespace annual
         static void Usage()
         {
             Console.WriteLine("Usage: annual [year] [directory]");
+        }
+
+        static List<String> GetYearDisplayNames(string year, string cnStr)
+        {
+            var ret = new List<String>();
+
+            var cn = new SqlConnection(cnStr);
+            cn.Open();
+            var cmd = new SqlCommand("getYearSubjects", cn) { CommandType = CommandType.StoredProcedure };
+            cmd.Parameters.Add(new SqlParameter("year", year));
+            var rdr = cmd.ExecuteReader();
+            while(rdr.Read())
+            {
+                ret.Add(rdr["name"].ToString());
+            }
+
+            return ret;
         }
 
     }
