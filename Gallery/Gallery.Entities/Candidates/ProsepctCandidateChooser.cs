f096e973c140920b15574633605dd1ebc0aa112a
Jake Bladt <jake@jakebladt.com>
2018-04-28 23:36:15 -0400

    +call

--- Gallery/Gallery.Entities/Candidates/ProsepctCandidateChooser.cs
+++ Gallery/Gallery.Entities/Candidates/ProsepctCandidateChooser.cs
@@ -1,19 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
 using Gallery.Entities.Subjects;
 
 namespace Gallery.Entities.Candidates
 {
     public class ProsepctCandidateChooser : ICandidateChooser
     {
-        public string Name => throw new NotImplementedException();
+        public string Name { get; set; }
+        protected string ConnectionString { get; set; }
+
+        public ProsepctCandidateChooser(string cn)
+        {
+            ConnectionString = cn;
+        }
 
         public List<ISubject> GetCandidates()
         {
-            throw new NotImplementedException();
+            var ret = new List<ISubject>();
+
+            var cn = new SqlConnection(ConnectionString);
+            cn.Open();
+
+            var cmd = new SqlCommand("getRookies", cn) { CommandType = CommandType.StoredProcedure };
+            var rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                var subject = new SqlBackedSubject
+                {
+                    ID = Convert.ToInt32(rdr["Id"]),
+                    Name = rdr["Name"].ToString(),
+                    DisplayName = rdr["DisplayName"].ToString(),
+                    ImageCount = Convert.ToInt32(rdr["ImageCount"])
+                };
+                ret.Add(subject);
+            }
+
+            return ret;
         }
     }
 }
