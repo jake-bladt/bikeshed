8e434b89f5258255eaa4d8c72525d49e96eff9d9
Jake Bladt <jake@jakebladt.com>
2018-04-28 23:32:58 -0400

    +chooser

---
+++ Gallery/Gallery.Entities/Candidates/ProsepctCandidateChooser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gallery.Entities.Subjects;
+
+namespace Gallery.Entities.Candidates
+{
+    public class ProsepctCandidateChooser : ICandidateChooser
+    {
+        public string Name => throw new NotImplementedException();
+
+        public List<ISubject> GetCandidates()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
