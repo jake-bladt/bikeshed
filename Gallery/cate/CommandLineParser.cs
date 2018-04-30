464426371264fcadb5d5c480e6ab8892d874f573
Jake Bladt <jake@jakebladt.com>
2018-04-30 19:55:58 -0400

    parse

--- Gallery/cate/CommandLineParser.cs
+++ Gallery/cate/CommandLineParser.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace cate
 {
-    class CommandLineParser
+    public class CommandLineParser
     {
+        public IEnumerable<String> Switches { get; protected set; }
+        public IEnumerable<String> Categories { get; protected set; }
+        public String SubjectName { get; protected set; } = String.Empty;
+
+        public CommandLineParser(string[] args)
+        {
+            SubjectName = args[0];
+            Switches = args.ToList().Where(a => a.StartsWith("-"));
+            Categories = args.ToList().Where(a => !a.StartsWith("-") && a != SubjectName);
+        }
     }
 }
