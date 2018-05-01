3b6e81ba314697adf04d9f7b0bf989d61abb0e89
Jake Bladt <jake@jakebladt.com>
2018-04-30 20:54:48 -0400

    +repo

---
+++ Gallery/Gallery.Entities/Taxonomy/SubjectCategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.Entities.Taxonomy
+{
+    class SubjectCategoryRepository
+    {
+        protected string _ConnectionString;
+
+        public SubjectCategoryRepository(string cnStr)
+        {
+            _ConnectionString = cnStr;
+        }
+
+
+
+    }
+}
