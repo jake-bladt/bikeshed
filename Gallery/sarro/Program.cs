d3ac4d8d9041285561202c62733a92a4cdebf8a1
Jake Bladt <jake@jakebladt.com>
2018-07-06 15:46:19 -0400

    fix modulo

--- Gallery/sarro/Program.cs
+++ Gallery/sarro/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Configuration;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Gallery.Entities.Candidates;
 using Gallery.Entities.Subjects;
@@ -16,14 +14,31 @@ namespace sarro
         {
             var runoffRoot = args[0];
             var targetCount = args.Length > 1 ? Int32.Parse(args[1]) : 30;
+            var yearbookHome = ConfigurationManager.AppSettings["yearbookLocation"];
 
             var cnStr = ConfigurationManager.ConnectionStrings["galleryDb"].ConnectionString;
             var chooser = new SuperannuatedRookieChooser(cnStr);
             var sars = chooser.GetCandidates();
             Console.WriteLine($"{sars.Count} SARs discovered.");
 
-            var pool = CandidatePool.FromListOfSubjects(sars);
+            var shuffled = sars.OrderBy(x => new Random().Next()).ToArray();
+            var roNum = 0;
+            DirectoryInfo roDi = null;
+            for(int i = 0; i < shuffled.Length; i++)
+            {
+                if(i == 0 || i % targetCount == 0)
+                {
+                     var roPath = $"{runoffRoot}{(++roNum).ToString("000")}";
+                    roDi = Directory.CreateDirectory(roPath);
+                }
+                var fn = $"{shuffled[i].DisplayName}.jpg";
+                var srcPath = Path.Combine(yearbookHome, fn);
+                var tarPath = Path.Combine(roDi.FullName, fn);
+                Console.WriteLine($"Copying {srcPath} to {tarPath}.");
+                // File.Copy(srcPath, tarPath);
+            }
 
+            Console.WriteLine("Operation complete.");
             Console.ReadLine();
         }
     }
