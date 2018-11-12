645acff7973a5a1e5e5c52fa89473fd71259e40c
Jake Bladt <jake@jakebladt.com>
2018-11-11 21:27:06 -0500

    +writer

---
+++ Gallery/ImageGallery.Migration/CategoryMigrationWriter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Gallery.Migration
+{
+    public class CategoryMigrationWriter : IDisposable
+    {
+
+        protected String _filePath;
+        protected StreamWriter _writer;
+
+        public CategoryMigrationWriter(String path)
+        {
+            _filePath = path;
+            _writer = new StreamWriter(_filePath, false);
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                _writer.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void WriteExportLine(String line)
+        {
+            _writer.WriteLine(line);
+        }
+
+    }
+}
