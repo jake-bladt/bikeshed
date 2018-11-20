e9aeedbb15c7eaf4f44b6343444af2a0247a810e
Jake Bladt <jake@jakebladt.com>
2018-11-20 08:04:11 -0500

    fix pattern

--- Gallery/mismatch/Program.cs
+++ Gallery/mismatch/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.IO;
 
 namespace mismatch
@@ -10,7 +11,19 @@ namespace mismatch
         static void Main(string[] args)
         {
             var subjectRoot = ConfigurationManager.AppSettings["subjectRoot"];
+            var subjectsDI = new DirectoryInfo(subjectRoot);
+            var subjectDirs = subjectsDI.GetDirectories();
 
+            var yearbookRoot = ConfigurationManager.AppSettings["yearbookRoot"];
+            var yearbookDI = new DirectoryInfo(yearbookRoot);
+            var yearbookFiles = yearbookDI.GetFiles("*.jpg");
+            var yearbookSubjectNames = yearbookFiles.ToList().Select(f => f.Name.Replace(".jpg", String.Empty)).ToList();
+
+            subjectDirs.ToList().ForEach(dir => {
+                if (!yearbookSubjectNames.Contains(dir.Name)) Console.WriteLine(dir.Name);
+            });
+            
+            Console.ReadLine();
 
         }
     }
