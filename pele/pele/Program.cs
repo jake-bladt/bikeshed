9ba5f243b47e4c3a56193ffd944c3f987f707c91
Jake Bladt <jake@jakebladt.com>
2019-10-27 19:46:30 -0400

    +pele

---
+++ pele/pele/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace pele
+{
+    class Program
+    {
+        public static string Path { get; set; }
+
+        public static void Usage()
+        {
+            Console.WriteLine("Usage: pele <directory-to-watch>");
+        }
+
+        static void Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Usage();
+                return;
+            }
+
+            Path = args[0];
+            var watcher = new FileSystemWatcher(Path);
+
+
+            Console.ReadLine();
+        }
+    }
+}
