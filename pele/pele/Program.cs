40e98d9830ca989102f30a3a3e6659944ff2ddaa
Jake Bladt <jake@jakebladt.com>
2019-10-27 21:23:58 -0400

    Check for dupes and unexpected ranks

--- pele/pele/Program.cs
+++ pele/pele/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace pele
 {
@@ -12,6 +15,63 @@ namespace pele
             Console.WriteLine("Usage: pele <directory-to-watch>");
         }
 
+        public static void ScanElection(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Change in {e.Name}.");
+
+            var di = new DirectoryInfo(Path);
+            var files = di.GetFiles("*.jpg");
+            var totalFileCount = files.Length;
+            Console.WriteLine($"{totalFileCount} files.");
+
+            var startsWithRank = @"^\d+";
+            var allFileList = files.ToList();
+            var rankedFiles = allFileList.Where(f => Regex.IsMatch(f.Name, startsWithRank));
+
+            var arr = rankedFiles.ToArray();
+            var rankedFileCount = arr.Length;
+            if (rankedFileCount > 0)
+            {
+                Console.WriteLine($"{rankedFileCount} ranked files.");
+                var expectedRanks = new List<int>();
+                for(int i = 0; i < rankedFileCount; i++)
+                {
+                    expectedRanks.Add(totalFileCount - i);
+                }
+
+                var errList = new List<string>();
+                rankedFiles.ToList().ForEach(f =>
+                {
+                    var r = Regex.Match(f.Name, startsWithRank).Value;
+                    var rank = int.Parse(r);
+                    if(expectedRanks.Contains(rank))
+                    {
+                        expectedRanks.Remove(rank);
+                    }
+                    else
+                    {
+                        var minExpectedRank = totalFileCount - rankedFileCount;
+                        if (rank >= minExpectedRank && rank <= totalFileCount)
+                        {
+                            errList.Add($"Duplicate rank {rank}.");
+                        } 
+                        else
+                        {
+                            errList.Add($"Unexpected rank {rank}.");
+                        }
+                    }
+
+                    if(errList.ToArray().Length > 0)
+                    {
+                        errList.ForEach(e => Console.WriteLine(e));
+                    }
+
+                });
+
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
             if(args.Length < 1)
@@ -21,8 +81,12 @@ namespace pele
             }
 
             Path = args[0];
+            Console.WriteLine($"Watching {Path}.");
             var watcher = new FileSystemWatcher(Path);
-
+            watcher.Created += ScanElection;
+            watcher.Deleted += ScanElection;
+            watcher.Renamed += ScanElection;
+            watcher.EnableRaisingEvents = true;
 
             Console.ReadLine();
         }
