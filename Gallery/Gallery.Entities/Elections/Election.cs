1ae98a645a7e67a51fc14da84782f8d5439bb4ff
Jake Bladt <jake@jakebladt.com>
2019-11-03 01:47:23 -0400

    -dead code

--- Gallery/Gallery.Entities/Elections/Election.cs
+++ Gallery/Gallery.Entities/Elections/Election.cs
@@ -12,46 +12,6 @@ namespace Gallery.Entities.Elections
 {
     public class Election : IElection, IElectionListing
     {
-        public static Election FromDirectory(
-            string dirPath,
-            string name,
-            DateTime date,
-            ElectionType eventType, 
-            IImageGallery gallery)
-        {
-
-            if (!Directory.Exists(dirPath)) throw new ArgumentException(String.Format("The directory {0} does not exist.", dirPath));
-
-            var di = new DirectoryInfo(dirPath);
-            var fis = di.GetFiles("*.jpg");
-            var winnerCount = fis.Count();
-            var winners = new Dictionary<int, ISubject>();
-
-            fis.ToList().ForEach(fi =>
-            {
-                var line = fi.Name;
-                var entries = NameMapper.ElectionFileNameToElectionEntry(line);
-                if (null == entries) return;
-                int pos = entries.Item1;
-                string winnerName = entries.Item2;
-
-                if (winners.ContainsKey(pos)) throw new DuplicateElectionEntryException(String.Format("Multiple entries at position {0}", pos));
-                if (!gallery.Subjects.ContainsKey(winnerName)) throw new UnknownSubjectException(String.Format("Unknown subject: {0}", winnerName));
-                var subject = gallery.Subjects[winnerName];
-                if (winners.ContainsValue(subject)) throw new DuplicateSubjectException(String.Format("Duplicate subject: {0}", winnerName));
-                winners[pos] = gallery.Subjects[winnerName];
-            });
-
-            for (int i = 1; i <= winnerCount; i++)
-            {
-                if (!winners.ContainsKey(i)) return null;
-            }
-
-            
-            var ret = new Election(name, date) { EventType = eventType, WinnerCount = winnerCount, Winners = winners };
-            return ret;
-        }
-
         public string Name { get; set; }
         public int Id { get; set; }
         public ElectionType EventType { get; set; }
